90773d8429f5f698ffea7799ddf4d3bae10eeb7c
SongMinJe <dlsgur3543@gmail.com>
2024-07-24 18:25:33 +0900

    Player_Move

---
+++ Assets/02.Scripts/Player/GroundCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    [Header("Boxcast Property")]
+    [SerializeField] private Vector3 boxSize;
+    [SerializeField] private LayerMask groundLayer;
+
+    [Header("Debug")]
+    [SerializeField] private bool drawGizmo;
+    public bool IsGrounded()
+    {
+        Collider[] hits = Physics.OverlapBox(transform.position, boxSize / 2, transform.rotation, groundLayer);
+        if (hits.Length > 0)
+        {
+            if (hits[0].CompareTag("Ground"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/02.Scripts/Player/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float playerSpeed = 2f;
+    [SerializeField]
+    private float jumpForce = 2f;
+    [SerializeField]
+    private float gravity = -9.81f;
+
+    private GroundCheck groundCheck;
+    private CharacterController cc;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private InputManager inputManager;
+    private void Start()
+    {
+        groundCheck = GetComponent<GroundCheck>();
+        cc = GetComponent<CharacterController>();
+        inputManager = InputManager.Instance;
+    }
+
+    private void Update()
+    {
+        groundedPlayer = groundCheck.IsGrounded();
+        if (groundedPlayer && playerVelocity.y <= 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movement = inputManager.GetPlayerMovement();
+        Vector3 dir = new Vector3(movement.x, 0f, movement.y);
+
+        if (dir.magnitude > 1f)
+        {
+            dir.Normalize();
+        }
+
+        cc.Move(dir * playerSpeed * Time.deltaTime);
+        if (inputManager.PlayerJumpedThisFrame() && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpForce * -1f * gravity);
+        }
+
+        playerVelocity.y += gravity * Time.deltaTime;
+        cc.Move(playerVelocity * Time.deltaTime);
+    }
+}
