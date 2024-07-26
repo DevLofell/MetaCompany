e697049b44ef1d82937f218c62aef117b1de53ad
lofell <lofell@naver.com>
2024-07-26 14:26:35 +0900

    콘솔 업데이트 상점 초안

--- Assets/02.Scripts/Ship/ConsoleSystem/ConsoleManager.cs
+++ Assets/02.Scripts/Ship/ConsoleSystem/ConsoleManager.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Threading;
+
+[System.Serializable]
+public class Command
+{
+    public string name;
+    public string description;
+    public string actionname;
+}
+
+[System.Serializable]
+public class CommandList
+{
+    public List<Command> commands;
+}
 
 public class ConsoleManager : MonoSingleton<ConsoleManager>
 {
+    private static readonly string COMMAND_FILE_PATH = Path.Combine(Application.streamingAssetsPath, "Data", "Command.json");
+
+    public CommandList commandList;
+
     [Header("프리팹")]
     public GameObject outputPrefab;
     public GameObject inputPrefab;
 
     [Header("타겟")]
     public Transform monitor;
+    public GameObject Spacer;
     public InputField inputField;
     private Text output;
 
@@ -18,8 +39,44 @@ public class ConsoleManager : MonoSingleton<ConsoleManager>
 
     private void Awake()
     {
+        LoadCommandList();
         SetupInitialConsole();
         RegisterCommands();
+        LoadAndDisplayStartScreen();
+    }
+
+    private void LoadCommandList()
+    {
+        if (File.Exists(COMMAND_FILE_PATH))
+        {
+            string jsonContent = File.ReadAllText(COMMAND_FILE_PATH, System.Text.Encoding.UTF8);
+            commandList = JsonUtility.FromJson<CommandList>(jsonContent);
+        }
+        else
+        {
+            Debug.LogError("Command.json file not found at path: " + COMMAND_FILE_PATH);
+            commandList = new CommandList();
+        }
+    }
+
+    private void LoadAndDisplayStartScreen()
+    {
+        if (commandList != null && commandList.commands != null)
+        {
+            Command startCommand = commandList.commands.Find(cmd => cmd.name == "consolestart");
+            if (startCommand != null)
+            {
+                PrintToConsole(startCommand.description);
+            }
+            else
+            {
+                PrintToConsole("Welcome to the console.");
+            }
+        }
+        else
+        {
+            PrintToConsole("Welcome to the console.");
+        }
     }
 
     private void Update()
@@ -32,6 +89,12 @@ public class ConsoleManager : MonoSingleton<ConsoleManager>
 
     private void SetupInitialConsole()
     {
+        if (Spacer == null)
+        {
+            Spacer = new GameObject("Spacer");
+            Spacer.transform.SetParent(monitor, false);
+            // Spacer에 필요한 컴포넌트 추가 (예: LayoutElement)
+        }
         ClearMonitor("");
     }
 
@@ -39,10 +102,16 @@ public class ConsoleManager : MonoSingleton<ConsoleManager>
     {
         for (int i = monitor.childCount - 1; i >= 0; i--)
         {
-            Destroy(monitor.GetChild(i).gameObject);
+            Transform child = monitor.GetChild(i);
+            if (child.gameObject != Spacer)
+            {
+                Destroy(child.gameObject);
+            }
         }
 
+        Debug.Log(message);
         GameObject outIns = Instantiate(outputPrefab, monitor);
+        outIns.transform.SetSiblingIndex(Spacer.transform.GetSiblingIndex() + 1);
         output = outIns.GetComponent<Text>();
         output.text = message;
 
@@ -63,18 +132,28 @@ public class ConsoleManager : MonoSingleton<ConsoleManager>
     private void RegisterCommands()
     {
         commands = new Dictionary<string, System.Action<string[]>>();
-        commands.Add("shop", OpenShop);
-        commands.Add("goto", GoToDestination);
     }
 
     private void ProcessCommand(string message)
     {
         string[] splitMessage = message.Split(' ');
-        string command = splitMessage[0];
+        string command = splitMessage[0].ToLower();
         string[] args = new string[splitMessage.Length - 1];
         System.Array.Copy(splitMessage, 1, args, 0, args.Length);
 
-        if (commands.ContainsKey(command))
+        if (command == "store")
+        {
+            StoreManager storeManager = FindObjectOfType<StoreManager>();
+            if (storeManager != null)
+            {
+                PrintToConsole(storeManager.GetFormattedItemList());
+            }
+            else
+            {
+                PrintToConsole("Store is currently unavailable.");
+            }
+        }
+        else if (commands.ContainsKey(command))
         {
             commands[command](args);
         }
@@ -84,25 +163,14 @@ public class ConsoleManager : MonoSingleton<ConsoleManager>
         }
     }
 
-    private void OpenShop(string[] args)
+    public string GetCommandDescription(string commandName)
     {
-        // 상점 기능 구현
-        PrintToConsole("Welcome to the shop!");
-        // 추가 상점 로직
-    }
-
-    private void GoToDestination(string[] args)
-    {
-        if (args.Length > 0)
+        if (commandList == null || commandList.commands == null)
         {
-            string destination = args[0];
-            PrintToConsole("Going to " + destination);
-            // 목적지 이동 로직
+            return "";
         }
-        else
-        {
-            PrintToConsole("Please specify a destination.");
-        }
+        Command command = commandList.commands.Find(cmd => cmd.name == commandName);
+        return command != null ? command.description : "";
     }
 
     public void PrintToConsole(string message)
---
+++ Assets/02.Scripts/Ship/ConsoleSystem/StoreManager.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public string description;
+    public int price;
+    public string prefabName;
+    public string upgradeType;
+    public string decorationType;
+}
+
+[System.Serializable]
+public class ItemList
+{
+    public List<Item> regularItems;
+    public List<Item> shipUpgrades;
+    public List<Item> specialItems;
+}
+
+public class StoreManager : MonoBehaviour
+{
+    public ItemList itemList;
+
+    void Start()
+    {
+        LoadItemList();
+    }
+
+    void LoadItemList()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath,"Data", "ItemList.json");
+        if (File.Exists(filePath))
+        {
+            string jsonContent = File.ReadAllText(filePath);
+            itemList = JsonUtility.FromJson<ItemList>(jsonContent);
+        }
+        else
+        {
+            Debug.LogError("ItemList.json not found!");
+        }
+    }
+
+    public string GetFormattedItemList()
+    {
+        string regularItemsStr = FormatItems(itemList.regularItems);
+        string shipUpgradesStr = FormatItems(itemList.shipUpgrades);
+        string specialItemsStr = FormatItems(itemList.specialItems);
+
+        return string.Format(ConsoleManager.instance.GetCommandDescription("store"),
+                             regularItemsStr, shipUpgradesStr, specialItemsStr);
+    }
+
+    private string FormatItems(List<Item> items)
+    {
+        string result = "";
+        foreach (var item in items)
+        {
+            result += $"* {item.name}  //  가격: ${item.price}\n";
+        }
+        return result;
+    }
+
+    public GameObject GetItemPrefab(string itemName)
+    {
+        Item item = FindItem(itemName);
+        if (item != null && !string.IsNullOrEmpty(item.prefabName))
+        {
+            return Resources.Load<GameObject>(item.prefabName);
+        }
+        return null;
+    }
+
+    private Item FindItem(string itemName)
+    {
+        foreach (var item in itemList.regularItems)
+        {
+            if (item.name == itemName) return item;
+        }
+        foreach (var item in itemList.shipUpgrades)
+        {
+            if (item.name == itemName) return item;
+        }
+        foreach (var item in itemList.specialItems)
+        {
+            if (item.name == itemName) return item;
+        }
+        return null;
+    }
+}
