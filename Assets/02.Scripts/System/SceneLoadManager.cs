86eb9fc014c0e26832a709d781fee77a05705d76
lofell <lofell@naver.com>
2024-08-03 20:50:44 +0900

    함선 수정

--- Assets/02.Scripts/System/SceneLoadManager.cs
+++ Assets/02.Scripts/System/SceneLoadManager.cs
@@ -2,30 +2,46 @@
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEditor.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public enum SceneList : int
 {
     Mainmenu = 0,
     Ship = 1,
-    
-
 }
 
+[System.Serializable]
+public class SceneInfo
+{
+    public string sceneName;
+    public int buildIndex;
+#if UNITY_EDITOR
+    public SceneAsset sceneAsset;
+#endif
+}
+
+[ExecuteInEditMode]
 public class SceneLoadManager : MonoSingleton<SceneLoadManager>
 {
     public static SceneLoadManager Instance { get; private set; }
-
-    [System.Serializable]
-    public class SceneInfo
-    {
-        public string sceneName;
-        public int buildIndex;
-    }
 
     public List<SceneInfo> sceneList = new List<SceneInfo>();
 
     private void Awake()
     {
+        if (!Application.isPlaying)
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            return;
+        }
+
         if (Instance == null)
         {
             Instance = this;
@@ -37,8 +53,31 @@ public class SceneLoadManager : MonoSingleton<SceneLoadManager>
         }
     }
 
+#if UNITY_EDITOR
+    public void LoadSceneInEditor(string sceneName)
+    {
+        SceneInfo sceneInfo = sceneList.Find(scene => scene.sceneName == sceneName);
+        if (sceneInfo != null && sceneInfo.sceneAsset != null)
+        {
+            EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(sceneInfo.sceneAsset));
+        }
+        else
+        {
+            Debug.LogError($"Scene '{sceneName}' not found in the list or scene asset is missing.");
+        }
+    }
+#endif
+
     public void LoadSceneByName(string sceneName)
     {
+        if (!Application.isPlaying)
+        {
+#if UNITY_EDITOR
+            LoadSceneInEditor(sceneName);
+#endif
+            return;
+        }
+
         SceneInfo sceneInfo = sceneList.Find(scene => scene.sceneName == sceneName);
         if (sceneInfo != null)
         {
@@ -52,6 +91,21 @@ public class SceneLoadManager : MonoSingleton<SceneLoadManager>
 
     public void LoadSceneByIndex(int index)
     {
+        if (!Application.isPlaying)
+        {
+#if UNITY_EDITOR
+            if (index >= 0 && index < sceneList.Count)
+            {
+                LoadSceneInEditor(sceneList[index].sceneName);
+            }
+            else
+            {
+                Debug.LogError($"Scene index {index} is out of range.");
+            }
+#endif
+            return;
+        }
+
         if (index >= 0 && index < sceneList.Count)
         {
             StartCoroutine(LoadSceneAsync(sceneList[index].buildIndex));
