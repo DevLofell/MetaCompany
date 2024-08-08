feaf99efa9ef765fed2281e0e90bc08b64f1847a
SongMinJe <dlsgur3543@gmail.com>
2024-08-08 21:04:51 +0900

    Item_SyncTool

---
+++ Assets/02.Scripts/Item/ItemDB/MainItemDatabase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class ItemsData
+{
+    public int ID;
+    public string Name;
+    public string Type;
+    public int kg;
+    public string hand;
+    public bool sound;
+    public bool interact;
+    public bool Conduction;
+}
+
+[CreateAssetMenu(fileName = "MainItemDatabase", menuName = "Inventory/MainItemDatabase")]
+public class MainItemDatabase : ScriptableObject
+{
+    public List<MainItemData> items = new List<MainItemData>();
+
+    public MainItemData GetItemById(int id)
+    {
+        return items.Find(item => item.ID == id);
+    }
+}
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    public int ID;
+    public string Name;
+    public string Type;
+    public int kg;
+    public string hand;
+    public bool sound;
+    public bool interact;
+    public bool Conduction;
+}
